918d64339ba2971170aabe2c5b528ef919a7ad97
David Davidov <kioltk@gmail.com>
2018-05-31 23:04:16 +0300

    Prettied

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Promise = require('bluebird').Promise
-const fs = Promise.promisifyAll(require('fs'))
-const iconv = require('iconv-lite')
+const Promise = require("bluebird").Promise;
+const fs = Promise.promisifyAll(require("fs"));
+const iconv = require("iconv-lite");
 
-const INT_SIZE = 4
-const SHORT_SIZE = 2
-const BYTE_SIZE = 1
+const INT_SIZE = 4;
+const SHORT_SIZE = 2;
+const BYTE_SIZE = 1;
 
 /**
  * Returns parsed JSON of Gravity Defield levels
@@ -12,143 +12,144 @@ const BYTE_SIZE = 1
  * @param {String} path Path to .mrg file
  * @returns {Object} Object contains title and pointers of map
  */
-module.exports = async (path) => {
-    const file = await fs.readFileAsync(path)
+module.exports = async path => {
+  const file = await fs.readFileAsync(path);
 
-    const levels = []
-    const levelsDescription = []
+  const levels = [];
+  const levelsDescription = [];
 
-    let offset = 0
-    // Parse levels description
-    for (let i = 0; i < 3; i++) {
-        // Level tracks count
-        const count = readInt(file, offset)
+  let offset = 0;
+  // Parse levels description
+  for (let i = 0; i < 3; i++) {
+    // Level tracks count
+    const count = readInt(file, offset);
 
-        const level = []
-        for (let i = 0; i < count; i++) {
-            const trackOffset = readInt(file)
-            let nameBytes = []
+    const level = [];
+    for (let i = 0; i < count; i++) {
+      const trackOffset = readInt(file);
+      let nameBytes = [];
 
-            // Read title bytes
-            while (nameBytes[nameBytes.length - 1] != 0x00) {
-                const byte = readByte(file, offset)
-                nameBytes.push(byte)
-            }
+      // Read title bytes
+      while (nameBytes[nameBytes.length - 1] != 0x00) {
+        const byte = readByte(file, offset);
+        nameBytes.push(byte);
+      }
 
-            // Parse title bytes into UTF-8 string
-            const titleBuf = new Buffer(nameBytes)
-            const title = iconv.encode(iconv.decode(titleBuf, "cp1251"), "utf8").toString();
+      // Parse title bytes into UTF-8 string
+      const titleBuf = new Buffer(nameBytes);
+      const title = iconv
+        .encode(iconv.decode(titleBuf, "cp1251"), "utf8")
+        .toString();
 
-            level.push({
-                offset: trackOffset,
-                title
-            })
+      level.push({
+        offset: trackOffset,
+        title
+      });
+    }
+
+    levelsDescription.push(level);
+  }
+
+  // Parse tracks
+  for (let i = 0; i < 3; i++) {
+    const level = [];
+
+    for (let j = 0; j < levelsDescription[i].length; j++) {
+      offset = levelsDescription[i][j].offset;
+
+      const mark = readByte(file, offset); // First byte of track, unused
+      const start = readIntCoordinates();
+      const finish = readIntCoordinates();
+      const pointsCount = readShort(file);
+
+      const first = readIntCoordinates(false);
+
+      const points = [first];
+      for (let k = 1; k < pointsCount - 1; k++) {
+        let point = readByteCoordinates(false);
+
+        if (point.x === -1) {
+          offset -= BYTE_SIZE;
+          points.push(readIntCoordinates(false));
+          continue;
         }
 
-        levelsDescription.push(level)
+        const { x: lastX, y: lastY } = points[points.length - 1];
+
+        points.push({
+          x: (point.x += lastX),
+          y: (point.y += lastY)
+        });
+      }
+
+      const track = {
+        title: levelsDescription[i][j].title,
+        start,
+        finish,
+        pointsCount,
+        points
+      };
+
+      level.push(track);
     }
 
-    // Parse tracks
-    for (let i = 0; i < 3; i++) {
-        const level = []
+    levels.push(level);
+  }
 
-        for (let j = 0; j < levelsDescription[i].length; j++) {
-            offset = levelsDescription[i][j].offset
+  return levels;
 
-            const mark = readByte(file, offset) // First byte of track, unused
-            const start = readIntCoordinates()
-            const finish = readIntCoordinates()
-            const pointsCount = readShort(file)
+  // HELPERS
 
-            const first = readIntCoordinates(false)
+  function readInt(buf) {
+    const int = buf.readIntBE(offset, INT_SIZE);
+    offset += INT_SIZE;
 
-            const points = [first]
-            for (let k = 1; k < pointsCount - 1; k++) {
-                let point = readByteCoordinates(false)
+    return int;
+  }
 
-                if (point.x === -1) {
-                    offset -= BYTE_SIZE
-                    points.push(readIntCoordinates(false))
-                    continue;
-                }
+  function readByte(buf) {
+    const byte = buf.readIntBE(offset, BYTE_SIZE);
+    offset += BYTE_SIZE;
 
-                const { x: lastX, y: lastY } = points[points.length - 1]
+    return byte;
+  }
 
-                points.push({
-                    x: point.x += lastX,
-                    y: point.y += lastY
-                })
-            }
+  function readShort(buf) {
+    const short = buf.readIntBE(offset, SHORT_SIZE);
+    offset += SHORT_SIZE;
 
-            const track = {
-                title: levelsDescription[i][j].title,
-                start,
-                finish,
-                pointsCount,
-                points
-            }
+    return short;
+  }
 
-            level.push(track)
-        }
-
-        levels.push(level)
+  function readIntCoordinates(decompress = true) {
+    if (decompress) {
+      return {
+        x: parseCoordinate(readInt(file)),
+        y: parseCoordinate(readInt(file))
+      };
+    } else {
+      return {
+        x: readInt(file),
+        y: readInt(file)
+      };
     }
+  }
 
-    return levels
-
-    // HELPERS
-
-    function readInt(buf) {
-        const int = buf.readIntBE(offset, INT_SIZE)
-        offset += INT_SIZE
-
-        return int
+  function readByteCoordinates(decompress = true) {
+    if (decompress) {
+      return {
+        x: parseCoordinate(readByte(file)),
+        y: parseCoordinate(readByte(file))
+      };
+    } else {
+      return {
+        x: readByte(file),
+        y: readByte(file)
+      };
     }
+  }
 
-    function readByte(buf) {
-        const byte = buf.readIntBE(offset, BYTE_SIZE)
-        offset += BYTE_SIZE
-
-        return byte
-    }
-
-    function readShort(buf) {
-        const short = buf.readIntBE(offset, SHORT_SIZE)
-        offset += SHORT_SIZE
-
-        return short
-    }
-
-    function readIntCoordinates(decompress = true) {
-        if (decompress) {
-            return {
-                x: parseCoordinate(readInt(file)),
-                y: parseCoordinate(readInt(file))
-            }
-        } else {
-            return {
-                x: readInt(file),
-                y: readInt(file)
-            }
-        }
-    }
-
-    function readByteCoordinates(decompress = true) {
-        if (decompress) {
-            return {
-                x: parseCoordinate(readByte(file)),
-                y: parseCoordinate(readByte(file))
-            }
-        } else {
-            return {
-                x: readByte(file),
-                y: readByte(file)
-            }
-        }
-    }
-
-    function parseCoordinate(number) {
-        return number << 3 >> 16
-    }
-}
-
+  function parseCoordinate(number) {
+    return (number << 3) >> 16;
+  }
+};
